2d31d48e936d7a35c9f6df70745afdadc14170b3
William <willgamedev@gmail.com>
2023-08-30 17:06:42 +0100

    cursor tweaks

--- Assets/Scripts/Game/VirtualCursor.cs
+++ Assets/Scripts/Game/VirtualCursor.cs
@@ -8,6 +8,7 @@ public class VirtualCursor : MonoBehaviour
     [SerializeField]
     private PlayerInput input;
     private Mouse virtualMouse;
+    private Mouse currentMouse;
 
     [SerializeField]
     private RectTransform cursorTransform;
@@ -18,13 +19,23 @@ public class VirtualCursor : MonoBehaviour
 
     [SerializeField]
     private float cursorSpeed = 1000f;
+    [SerializeField]
+    private float padding = 35f;
 
     private bool previousMouseState;
     private Camera cam;
 
+    private string previousControls = "";
+    private const string GPScheme = "Gamepad";
+    private const string MouseScheme = "Keyboard&Mouse";
+
+
     private void OnEnable()
     {
         cam = Camera.main;
+        currentMouse = Mouse.current;
+
+        InputDevice virtualMouseInputDevice = InputSystem.GetDevice("VirtualMouse");
 
         if (virtualMouse == null)
         {
@@ -33,6 +44,10 @@ public class VirtualCursor : MonoBehaviour
         else if(!virtualMouse.added)
         {
             InputSystem.AddDevice(virtualMouse);
+        }
+        else
+        {
+            virtualMouse = (Mouse)virtualMouseInputDevice;
         }
 
         InputUser.PerformPairingWithDevice(virtualMouse, input.user);
@@ -44,11 +59,38 @@ public class VirtualCursor : MonoBehaviour
         }
 
         InputSystem.onAfterUpdate += UpdateMotion;
+        input.onControlsChanged += ChangeControls;
     }
 
     private void OnDisable()
     {
+        if (virtualMouse != null && virtualMouse.added)
+        {
+            InputSystem.RemoveDevice(virtualMouse);
+        }
         InputSystem.onAfterUpdate -= UpdateMotion;
+        input.onControlsChanged -= ChangeControls;
+    }
+
+    private void ChangeControls(PlayerInput inputs)
+    {
+        if (input.currentControlScheme == MouseScheme && previousControls != MouseScheme)
+        {
+            cursorTransform.gameObject.SetActive(false);
+            Cursor.visible = true;
+            currentMouse.WarpCursorPosition(virtualMouse.position.ReadValue());
+            previousControls = MouseScheme;
+        }
+
+        if (input.currentControlScheme == GPScheme && previousControls != GPScheme)
+        {
+            cursorTransform.gameObject.SetActive(true);
+            Cursor.visible = false;
+            InputState.Change(virtualMouse.position, currentMouse.position.ReadValue());
+            AnchorCursor(currentMouse.position.ReadValue());
+            previousControls = GPScheme;
+
+        }
     }
 
     private void UpdateMotion()
@@ -64,8 +106,8 @@ public class VirtualCursor : MonoBehaviour
         Vector2 currentPosition = virtualMouse.position.ReadValue();
         Vector2 newPosition = currentPosition + deltaValue;
 
-        newPosition.x = Mathf.Clamp(newPosition.x, 0, Screen.width);
-        newPosition.y = Mathf.Clamp(newPosition.y, 0, Screen.height);
+        newPosition.x = Mathf.Clamp(newPosition.x, padding, Screen.width - padding);
+        newPosition.y = Mathf.Clamp(newPosition.y, padding, Screen.height - padding);
 
         InputState.Change(virtualMouse.position, newPosition);
         InputState.Change(virtualMouse.delta, deltaValue);
